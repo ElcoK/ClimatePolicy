80cc19b57837a265ac22f76274f8200b2381be3f
elco.k <elcokoks@gmail.com>
2019-09-06 07:36:55 +0200

    latest

--- Notebooks/DamageScanner.py
+++ Notebooks/DamageScanner.py
@@ -2,6 +2,87 @@
 import rasterio
 import numpy
 import pandas 
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.patches import Patch
+import matplotlib.pyplot as plt
+from rasterio.plot import show
+
+def plot_landuse(landuse_ras,color_dict={},save=False,**kwargs):
+    """
+    Arguments:
+
+        *landuse_map* : path to GeoTiff with land-use information per grid cell. 
+    Optional Arguments:
+        *color_dict* : Supply a dictionary with the land-use classes as keys 
+        and color hex codes as values. If empty, it will use a default list (which will probably fail)
+        
+        *save* : Set to True if you would like to save the output. Requires 
+        several **kwargs**
+        
+    kwargs:
+        *output_path* : Specify where files should be saved.
+        
+        *scenario_name*: Give a unique name for the files that are going to be saved.
+    """
+    fig, ax = plt.subplots(1, 1,figsize=(12,10))
+
+    if len(color_dict) == 0:
+        color_dict = {
+        110 : '#fb897e' ,     111 : '#b40e3e' ,     112 : '#ee0000' ,     120 : '#ee0000' , 
+        130 : '#edc3c3' ,     131 : '#d97489' ,     133 : '#da6c99' ,     134 : '#da6c99' , 
+        135 : '#da6c99' ,     136 : '#da6c99' ,     140 : '#331e36' ,     141 : '#363b74' , 
+        142 : '#363b74' ,     143 : '#363b74' ,     144 : '#363b74' ,     150 : '#69868A' , 
+        151 : '#363b74' ,     152 : '#363b74' ,     210 : '#fcfcb9' ,     211 : '#fcfcb9' , 
+        220 : '#B59E99' ,     221 : '#7b323d' ,     230 : '#c3eead' ,     240 : '#b29600' , 
+        250 : '#b29600' ,     310 : '#89ec46' ,     320 : '#89ec46' ,     330 : '#89ec46' , 
+        340 : '#89ec46' ,     350 : '#89ec46' ,     360 : '#1c7426' ,     370 : '#e7d7bf' , 
+        380 : '#3bbbb3' ,     410 : '#010000' ,     430 : '#71818e' ,     440 : '#c0c0c0' , 
+        450 : '#c0c0c0' ,     460 : '#c0c0c0' ,     520 : '#545454' ,     530 : '#c39797' , 
+        550 : '#7b323d' ,     560 : '#c0c0c0' ,    630 : '#b0e0e6' ,      640 : '#b0e0e6' , 
+        911 : '#b0e0e6' ,    -9999: '#ffffff'
+        }
+
+    with rasterio.open(landuse_ras) as src:
+        landuse = src.read()[0,:,:]
+        transform = src.transform
+
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_axis_off()
+        color_scheme_map = list(color_dict.values())
+
+        cmap = LinearSegmentedColormap.from_list(name='landuse',
+                                             colors=color_scheme_map)  
+
+        map_dict = dict(zip(color_dict.keys(),[x for x in range(len(color_dict))]))
+        # Function to be vectorized
+        def map_func(val, dictionary):
+            return dictionary[val] if val in dictionary else val 
+
+        # Vectorize map_func
+        vfunc  = numpy.vectorize(map_func)
+
+        # Run
+        landuse = vfunc(landuse, map_dict)
+
+        if 'background' in kwargs:
+            show(landuse,ax=ax,cmap=cmap,transform=transform,alpha=0.5)
+        else:
+            show(landuse,ax=ax,cmap=cmap,transform=transform)
+ 
+    if save:
+        if 'output_path' in kwargs:
+            output_path = kwargs['output_path']
+            if not os.path.exists(output_path):
+                os.mkdir(output_path)
+        if 'scenario_name' in kwargs:
+            scenario_name = kwargs['scenario_name']
+
+        fig.tight_layout()
+        fig.savefig(os.path.join(output_path,'landuse_{}.png'.format(scenario_name)),dpi=350, bbox_inches='tight')
+
+
+    return ax
 
 def DamageScanner(landuse_map,inun_map,curve_path,maxdam_path):
     """
